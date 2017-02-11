4b9e51134f1afb052cb88c6da8787ce0df11043b
cat <d1v1nation.code@gmail.com>
2017-02-11 19:57:04 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include "signal.h"
+
+signal<int(void)>* backref;
+signal<int(int, int)>* facref;
+
+template <int Num>
+int printNum() {
+    std::cout << "printNum " << Num << '\n';
+    return 42;
+}
+
+int reentreg() {
+    backref->connect(printNum<3>);
+    return -42;
+}
+
+template <int Num>
+struct funobj {
+    signal<int(void)>::connection* conn;
+
+    int operator()() {
+        std::cout << "funobj<" << Num << ">()" << '\n';
+
+        if (conn != nullptr) {
+            std::cout << "trying to disconnect " << conn->get_uniq_id() << '\n';
+
+            conn->disconnect();
+        }
+        return 24;
+    }
+};
+
+int factorisig(int arg, int acc) {
+    if (arg == 1)
+        return acc;
+    else
+        return (*facref)(arg - 1, arg * acc);
+}
+
+int main() {
+    signal<int(void)> s;
+    backref = &s;
+
+    s.connect(printNum<1>);
+    auto s1 = s.connect(printNum<2>);
+
+    s();
+    std::cout << '\n';
+
+    s1.disconnect();
+
+    s();
+    std::cout << '\n';
+
+    s.connect(reentreg);
+
+    s();
+    std::cout << '\n';
+
+    s();
+    std::cout << '\n';
+
+
+    signal<int(void)> new_sig;
+
+    funobj<6> o1;
+
+    auto c1 = new_sig.connect(o1);
+
+    funobj<7> o2 {&c1};
+
+    auto c2 = new_sig.connect(o2);
+
+    funobj<8> o3 {&c2};
+
+    auto c3 = new_sig.connect(o3);
+
+
+    new_sig();
+
+    std::cout << '\n';
+
+    new_sig();
+
+
+    signal<int(int, int)> fs;
+    facref = &fs;
+
+    fs.connect(factorisig);
+    std::cout << "factorisig! " << fs(5, 1) << '\n';
+
+}
---
+++ signal.h
@@ -0,0 +1,138 @@
+//
+// Created by cat on 11.02.17.
+//
+
+#ifndef SIGNAL_SIGNAL_H
+#define SIGNAL_SIGNAL_H
+
+#include <list>
+#include <functional>
+#include <queue>
+
+template <typename R>
+struct signal;
+
+template <typename R, typename... Args>
+struct signal<R(Args...)> {
+
+    typedef typename std::function<R(Args...)> callback_tp;
+
+    struct connection {
+        bool operator==(connection const & other) const {
+            return other.uniq_id == uniq_id;
+        }
+
+        connection(connection const &other) = default;
+        connection(connection &&other) = default;
+
+        bool is_alive() {
+            return parent.query_liveness(uniq_id);
+        }
+
+        void disconnect() {
+            parent.enq_dc(uniq_id);
+        }
+
+        signal get_parent() {
+            return parent;
+        }
+
+        int get_uniq_id() {
+            return uniq_id;
+        }
+
+    private:
+        callback_tp cback;
+        signal& parent;
+        int uniq_id;
+
+        template <typename F>
+        connection(F f, int lp, signal& sig) : cback(std::move(f)), uniq_id(lp), parent(sig) {};
+
+        friend class signal;
+    };
+
+    signal() : callbacks(), entrancy(false), cnt(0) {};
+
+    template <typename F>
+    connection connect(F func) {
+        connection c(func, cnt++, *this);
+
+        if (!entrancy) {
+            callbacks.push_back(c);
+        } else {
+            add_q.push_back(c);
+        }
+
+        return c;
+    }
+
+    // does not work for void
+    // one could specify
+
+    R operator()(Args... args) {
+        bool entrancy = true;
+        R result;
+
+        auto ae = --callbacks.end();
+        for (auto it = callbacks.begin(); it != ae; it++){
+            (*it).cback(std::forward<Args>(args)...);
+        }
+
+        result = (*ae).cback(std::forward<Args>(args)...);
+
+        entrancy = false;
+
+        pop_qs();
+
+        return result;
+    }
+
+private:
+    std::list<connection> callbacks;
+    std::vector<int> rm_q;
+    std::vector<connection> add_q;
+    bool entrancy;
+    int cnt = 0;
+
+    friend class connection;
+
+    bool query_liveness(int conn_id) {
+        for (connection& c : callbacks) {
+            if (c.uniq_id == conn_id)
+                return true;
+        }
+
+        return false;
+    }
+
+    void enq_dc(int conn_id) {
+        if (!entrancy) {
+            find_and_remove(conn_id);
+        } else {
+            rm_q.push_back(conn_id);
+        }
+    }
+
+    void find_and_remove(int conn_id) {
+        for (typename std::list<connection>::iterator it = callbacks.begin(); it != callbacks.end(); it++) {
+                if ((*it).uniq_id == conn_id) {
+                    callbacks.erase(it);
+                    return;
+                }
+        }
+    }
+
+    void pop_qs() {
+        for (int id : rm_q) {
+            find_and_remove(id);
+        }
+
+        for (connection& c : add_q) {
+            callbacks.emplace_back(std::move(c));
+        }
+    }
+
+};
+
+#endif //SIGNAL_SIGNAL_H
